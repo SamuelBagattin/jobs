03d804a2f631d491d3b1c31cc1f6986b7f6bb4a4
Samuel Bagattin <samuel.bagattin@gmail.com>
2021-05-28 17:55:00 +0200

    Refacto to scrap on github actions

---
+++ jobs_scaper/entrypoints/sqs_worker/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sns"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	log "github.com/sirupsen/logrus"
+	"jobs_scaper/pkg/aws_sts"
+	"jobs_scaper/pkg/env"
+	"jobs_scaper/pkg/scraping"
+	"jobs_scaper/pkg/upload"
+)
+
+func main() {
+
+	log.SetLevel(log.TraceLevel)
+	log.Debug("Init")
+
+	sess := aws_sts.InitSession()
+	ssmClient := ssm.New(sess)
+	sqsClient := sqs.New(sess)
+	snsClient := sns.New(sess)
+
+	s3ScrapingClient := upload.NewS3Client(sess)
+
+	for {
+		var sqsQueueUrl = env.GetSqsQueueUrl()
+		log.Trace("Polling")
+		message, err := sqsClient.ReceiveMessage(&sqs.ReceiveMessageInput{
+			QueueUrl:            sqsQueueUrl,
+			MaxNumberOfMessages: aws.Int64(1),
+			MessageAttributeNames: []*string{
+				aws.String("All"),
+			},
+			WaitTimeSeconds: aws.Int64(20),
+		},
+		)
+		if err != nil {
+			panic(err)
+		}
+		for _, m := range message.Messages {
+			fmt.Printf("%v", m)
+
+			scrapingRes := scraping.ScrapeAll(ssmClient, s3ScrapingClient)
+
+			deleteMessage, err := sqsClient.DeleteMessage(&sqs.DeleteMessageInput{
+				QueueUrl:      sqsQueueUrl,
+				ReceiptHandle: message.Messages[0].ReceiptHandle,
+			})
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("%v", deleteMessage)
+
+			postScrapingErr := postScraping(scrapingRes, snsClient)
+			if postScrapingErr != nil {
+				return
+			}
+		}
+	}
+}
+
+func postScraping(queriesSummary *scraping.FinalScrapingResult, snsClient *sns.SNS) error {
+	stringMessage, errMarshal := json.Marshal(queriesSummary)
+	if errMarshal != nil {
+		return errMarshal
+	}
+	_, err := snsClient.Publish(&sns.PublishInput{
+		Message:  aws.String(string(stringMessage)),
+		TopicArn: env.GetSnsTopicDestinationArn(),
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- jobs_scaper/pkg/scraping/common.go
+++ jobs_scaper/pkg/scraping/common.go
@@ -1,7 +1,14 @@
 package scraping
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	log "github.com/sirupsen/logrus"
+	"jobs_scaper/pkg/upload"
 	"net/url"
+	"sync"
 	"time"
 )
 
@@ -29,4 +36,73 @@ type Client interface {
 
 type FinalScrapingResult struct {
 	QueriesStatistics map[string]*int `json:"queries_statistics"`
+}
+
+func ScrapeAll(ssmClient *ssm.SSM, s3ScrapClient *upload.S3ScrapingClient) *FinalScrapingResult {
+	log.Debug("Getting jobs-scraping-queries parameter")
+	param, err := ssmClient.GetParameter(&ssm.GetParameterInput{
+		Name: aws.String("jobs-scraping-queries"),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	var queries []string
+	if unmarshalErr := json.Unmarshal([]byte(*param.Parameter.Value), &queries); unmarshalErr != nil {
+		panic(unmarshalErr)
+	}
+	var queriesSummary = make(map[string]*int)
+	for _, query := range queries {
+		queriesSummary[query] = scrapeQuery(query, s3ScrapClient)
+	}
+	return &FinalScrapingResult{QueriesStatistics: queriesSummary}
+}
+
+func scrapeQuery(query string, s3ScrapClient *upload.S3ScrapingClient) *int {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	log.SetLevel(log.TraceLevel)
+
+	var scrapingClients []Client
+
+	var linkedInClient, nLinkedInClientErr = NewLinkedinClient()
+	if nLinkedInClientErr != nil {
+		panic(nLinkedInClientErr)
+	}
+
+	scrapingClients = append(scrapingClients, linkedInClient)
+
+	var monsterClient, nMonsterClientErr = NewMonsterClient()
+	if nMonsterClientErr != nil {
+		panic(nMonsterClientErr)
+	}
+
+	scrapingClients = append(scrapingClients, monsterClient)
+
+	var wg sync.WaitGroup
+	var jobsCount = 0
+	for _, client := range scrapingClients {
+		wg.Add(1)
+		client := client
+		go func() {
+			defer wg.Done()
+			res, err := client.Scrape(query)
+			if err != nil {
+				panic(err)
+			}
+
+			b, err := json.Marshal(res)
+			if err != nil {
+				log.Println("failed to serialize response:", err)
+				return
+			}
+			s3ScrapClient.UploadToFileByte(client.GetConfig().SiteName+url.QueryEscape(query), b, "application/json")
+			jobsCount += len(*res)
+		}()
+	}
+	wg.Wait()
+	return &jobsCount
 }
